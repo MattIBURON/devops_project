e5d88f18dc3e76bb8c4ef7d9bef03956d7ec2657
matthieu-at-742537929214 <matthieuribaucourt@yahoo.fr>
2024-05-06 21:45:41 +0200

    correct problems

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,7 +10,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  token = data.google_client_config.current.access_token
+  token = google_container_cluster.default.endpoint
   client_certificate = base64decode(google_container_cluster.default.master_auth[0].client_certificate)
   cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth[0].cluster_ca_certificate)
 }
