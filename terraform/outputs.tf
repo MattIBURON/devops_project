bd68763ea835224d9d38c0d40d899b9ee0f9528f
matthieu-at-742537929214 <matthieuribaucourt@yahoo.fr>
2024-05-06 15:22:02 +0200

    correct problems

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,5 +8,5 @@ output "cluster_location" {
   value = google_container_cluster.default.location
 }
 output "load-balancer-ip" {
-  value = value = google_container_cluster.default.address
+  value = google_container_cluster.default.address
 }
