c5f02d675f61026319c914cfe78ff4e9d9ca39c6
matthieu-at-742537929214 <matthieuribaucourt@yahoo.fr>
2024-05-06 19:34:59 +0200

    correct problems

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,14 +3,21 @@ terraform {
   backend "gcs" {
   }
 }
-   
+
 provider "google" {
   project = var.project_id
-  region = var.region
+  region  = var.region
 }
 
-provider "kubernetes" { 
-  token = data.google_client_config.current.access_token
-  client_certificate = base64decode(google_container_cluster.default.master_auth[0].client_certificate)
+provider "kubernetes" {
+  token                  = data.google_client_config.current.access_token
+  client_certificate     = base64decode(google_container_cluster.default.master_auth[0].client_certificate)
   cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+}
+
+module "kubernetes_resources" {
+  source          = "./module"
+  container_image = "us.gcr.io/cicd-terraform-422508/nodeappimage:${github.sha}"
+  region          = var.region
+  project_id      = var.project_id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,11 @@
-variable "project_id" {
-}
-variable "region" {
-}
 variable "container_image" {
+  description = "The container image for the Kubernetes deployment"
+}
+
+variable "region" {
+  description = "The region for the Google Cloud Platform resources"
+}
+
+variable "project_id" {
+  description = "The ID of the Google Cloud Platform project"
 }
